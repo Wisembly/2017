eafa8b1beedd6d15ddfdc2f780701e90922a3490
Gabriel Cousin <csngab@gmail.com>
2016-12-30 11:33:24 +0100

    Update build tasks and deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,53 @@
-var browserSync   = require('browser-sync');
-var reload        = browserSync.reload;
-
+var browserSync   = require('browser-sync').create();
 var gulp          = require('gulp');
-var plumber       = require('gulp-plumber');
-var rename        = require('gulp-rename');
+var pug           = require('gulp-pug');
 var sass          = require('gulp-sass');
-var autoprefixer  = require('gulp-autoprefixer');
-var cssmin        = require('gulp-cssmin');
+var base64        = require('gulp-base64');
+var prefix        = require('gulp-autoprefixer');
+var rename        = require('gulp-rename');
 
+var onError = function(err) {
+  console.log(err);
+  this.emit('end');
+}
 
-// BrowserSync parameters
-gulp.task('browser-sync', function() {
-    browserSync.init(["src/scss/**/*.scss", "*.html"], {
-        notify: false,
-        server: {
-            baseDir: "./"
-        },
-        reloadDelay: 500
-    });
+gulp.task('browser-sync', ['sass'], function() {
+  browserSync.init({
+    ui: false,
+    ghostMode: false,
+    server: "dist"
+  });
+
+  gulp.watch("dist/**/*.html").on("change", browserSync.reload)
 });
 
-
-// Dev mode: Sass + Autoprefixer
-gulp.task('dev', function(){
-	return gulp.src('src/scss/*.scss')
-        .pipe(plumber())
-        .pipe(sass({
-            errLogToConsole: true
-        }))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(rename('style.css'))
-		.pipe(gulp.dest('dist/css'))
-        .pipe(reload({
-            stream: true
-            }));
+gulp.task('pug', function () {
+  return gulp.src('src/pug/**/*.pug')
+    .pipe(pug({ pretty: "  " }))
+    .on('error', onError)
+    .pipe(gulp.dest('dist/'));
 });
 
-// Prod mode: Sass + Autoprefixer + Minifier
-gulp.task('prod', function(){
-	return gulp.src('src/scss/*.scss')
-		.pipe(plumber())
-		.pipe(sass())
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(cssmin({
-        		keepSpecialComments: '0'
-        	}))
-        	.pipe(rename('style.min.css'))
-		.pipe(gulp.dest('dist/css'));
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/style.scss')
+		.pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(base64({
+        extensions: ['svg'],
+        maxImageSize: false
+      }))
+    .pipe(prefix({
+      browsers: ['Safari 7', 'last 2 versions']
+    }))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
 });
 
-// Default task = `dev` + LiveReload
-gulp.task('default', ['dev', 'browser-sync'], function() {
-    gulp.watch(["src/scss/**/*.scss", "*.html"], ['dev']);
+gulp.task('build', ['pug', 'sass']);
+
+gulp.task('default', ['pug', 'sass', 'browser-sync'], function () {
+  gulp.watch('**/*.scss', ['sass']);
+  gulp.watch('**/*.pug', ['pug']);
 });
